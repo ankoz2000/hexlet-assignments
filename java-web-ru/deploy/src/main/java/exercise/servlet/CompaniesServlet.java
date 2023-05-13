e6a0b23c4abe0291a17db9f255911c633e861945
@hexlet/cli <support@hexlet.io>
2023-05-13 20:30:59 +0300

    submit java-web-ru/deploy

---
+++ java-web-ru/deploy/src/main/java/exercise/servlet/CompaniesServlet.java
@@ -0,0 +1,42 @@
+package exercise.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import static exercise.Data.getCompanies;
+
+public class CompaniesServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+                throws IOException, ServletException {
+
+        // BEGIN
+        List<String> companies = getCompanies();
+        List<String> result = new ArrayList<>();
+        PrintWriter pw = new PrintWriter(response.getWriter());
+        if (request.getRequestURI().contains("search")) {
+            String toContains = request.getParameter("search");
+            if (toContains == null) {
+                result.add("Companies not found");
+            } else {
+                result = companies.stream()
+                        .filter(c -> c.contains(toContains))
+                        .collect(Collectors.toList());
+            }
+        } else {
+            result = companies;
+        }
+        pw.println(result.stream()
+                .collect(Collectors.joining(", ")));
+        // END
+    }
+}
