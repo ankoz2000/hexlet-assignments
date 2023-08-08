781dbb40d526c36eb71cbe288771cc219c1f7f1b
@hexlet/cli <support@hexlet.io>
2023-08-08 10:46:43 +0300

    submit java-spring-ru/migrations

---
+++ java-spring-ru/migrations/src/main/java/exercise/controller/PeopleController.java
@@ -0,0 +1,38 @@
+package exercise.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/people")
+@RequiredArgsConstructor
+public class PeopleController {
+
+
+    private final JdbcTemplate jdbc;
+
+
+    @PostMapping(path = "")
+    public void createPerson(@RequestBody Map<String, Object> person) {
+        String query = "INSERT INTO person (first_name, last_name) VALUES (?, ?)";
+        jdbc.update(query, person.get("first_name"), person.get("last_name"));
+    }
+
+    // BEGIN
+    @GetMapping(path = "/{id}")
+    public void createPerson(@PathVariable Integer id) {
+        String query = "SELECT * FROM person WHERE id = ?";
+        jdbc.get(query, id);
+    }
+
+    // END
+}
